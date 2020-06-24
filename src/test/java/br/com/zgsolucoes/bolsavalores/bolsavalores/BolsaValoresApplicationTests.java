7736943b74df70756e6e1477e46c44b8a6574cc0
Andrey França <andrey.franca@totvs.com.br>
2020-06-24 19:14:48 -0300

    Initial commit

---
+++ src/test/java/br/com/zgsolucoes/bolsavalores/bolsavalores/BolsaValoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zgsolucoes.bolsavalores.bolsavalores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsaValoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
