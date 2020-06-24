ce166b214d5b85907ee0828d548cc03b4d05cb8e
Andrey França <andrey.franca@totvs.com.br>
2020-06-24 19:39:36 -0300

    Estrutura  inicial projeto

---
+++ src/main/java/br/com/zgsolucoes/bolsavalores/bolsavalores/api/v1/AtivoController.java
@@ -0,0 +1,7 @@
+package br.com.zgsolucoes.bolsavalores.bolsavalores.api.v1;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AtivoController {
+}
---
+++ src/main/java/br/com/zgsolucoes/bolsavalores/bolsavalores/repository/AtivoRepository.java
@@ -0,0 +1,7 @@
+package br.com.zgsolucoes.bolsavalores.bolsavalores.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AtivoRepository {
+}
