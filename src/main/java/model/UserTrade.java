c45bddefc3b48ee10fe620a27620ce835fffc0c9
Tiago Vitorino <tyagov@hotmail.com>
2020-06-24 19:46:15 -0300

    add modelagem do banco

---
+++ src/main/java/model/UserTrade.java
@@ -0,0 +1,103 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity(name = "user_trade")
+public class UserTrade implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Date data;
+    private String tipoOperacao;
+    private String mercado;
+    private String prazo;
+    private String instrument;
+    private String especificacao;
+    private Integer quantidade;
+    private Double preco;
+    private Double valorTotal;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getTipoOperacao() {
+        return tipoOperacao;
+    }
+
+    public void setTipoOperacao(String tipoOperacao) {
+        this.tipoOperacao = tipoOperacao;
+    }
+
+    public String getMercado() {
+        return mercado;
+    }
+
+    public void setMercado(String mercado) {
+        this.mercado = mercado;
+    }
+
+    public String getPrazo() {
+        return prazo;
+    }
+
+    public void setPrazo(String prazo) {
+        this.prazo = prazo;
+    }
+
+    public String getInstrument() {
+        return instrument;
+    }
+
+    public void setInstrument(String instrument) {
+        this.instrument = instrument;
+    }
+
+    public String getEspecificacao() {
+        return especificacao;
+    }
+
+    public void setEspecificacao(String especificacao) {
+        this.especificacao = especificacao;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+}
